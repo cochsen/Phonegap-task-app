c31b9e5589a963d958531ed7eb9e82f2cc167500
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2014-07-07 23:32:10 -0400

    Expanded HTM in .append()

--- js/app.js
+++ js/app.js
@@ -13,11 +13,31 @@ $(document).ready(function() {
     counter++;
     $('#task-list ul').empty();
     for(var i = 0; i < tasks.length; i++) {
-      $(document.body).append('<div data-role="page" id="page-' + i + '"><div><h2 class="ui-bar ui-bar-a title">Task List</h2></div><div class="ui-body ui-body-a"><form><fieldset data-iconpos="right"><label><input type="checkbox" id="sample-task" class="check-box" value="">' + tasks[i].name + '</label></fieldset><img src="https://s3.amazonaws.com/FileHelper/text-file-icon.png" class="task-img" /></form><div id="task-content" class="ui-body ui-body-a"></div><a href="#page-main" class="ui-btn ui-shadow">Back</a></div>');
+      $(document.body).append('<div data-role="page" id="page-' + i + '">' + 
+                                '<div>' + 
+                                  '<h2 class="ui-bar ui-bar-a title">Task List</h2>' +
+                                '</div>' + 
+                                '<div id="task-content" class="ui-body ui-body-a">' + 
+                                  '<div class="add-step-btn-div">' + 
+                                    '<a href="#page-add-step" id="add-step-btn" class="ui-btn ui-btn-inline ui-btn-icon-left ui-icon-plus">Add Step</a>' + 
+                                  '</div>' + 
+                                  '<form>' + 
+                                    '<img src="https://s3.amazonaws.com/FileHelper/text-file-icon.png" class="task-img" />' + 
+                                    '<fieldset data-iconpos="right">' + 
+                                      '<label>' + 
+                                        '<input type="checkbox" id="sample-task" class="check-box" value="">' + tasks[i].name + 
+                                      '</label>' + 
+                                    '</fieldset>' + 
+                                  '</form>' + 
+                                  '<a href="#page-main" class="ui-btn ui-shadow">Back</a>' + 
+                                '</div>' + 
+                              '</div>');
       $('#task-list ul').append('<li id="task-' + i.toString() + '" class="task-link" data-theme="a"><a href="#page-' + i + '">' + tasks[i].name + '</a></li>');
     }
     $('#task-list ul').listview("refresh");
   });
+  
+  
   
 });
 
