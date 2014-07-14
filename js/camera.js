f0100ee92f08f2a80c5d16713de4571b269b455c
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2014-07-14 12:35:15 -0400

    changed capturePhotoEdit()
    changed method from onPhotoURISuccess to onPhotoDataSuccess

--- js/camera.js
+++ js/camera.js
@@ -71,7 +71,7 @@ var destinationType; // sets the format of returned value
     //
     function getPhoto(source) {
       // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 40,
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 40,
         destinationType: destinationType.FILE_URI,
         sourceType: source });
     }
