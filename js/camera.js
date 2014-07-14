a93540c8853d32dcfd80e8bc2ccbc2ad5d169cce
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2014-07-14 15:33:41 -0400

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -72,7 +72,7 @@ var destinationType; // sets the format of returned value
     function getPhoto(source) {
       // Retrieve image file location from specified source
       navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 40,
-        destinationType: destinationType.DATA_URL
+        destinationType: destinationType.DATA_URL });
     }
 
     // Called if something bad happens.
